b53928624396e555c5f764d4ccd26cea14970086
DEREK_JOAQUIN_GALINDO_OTERO <a.20308051280494@cbtis128.edu.mx>
2024-03-13 14:50:36 +0000

    0494

--- lib/pantalla1_0494.dart
+++ lib/pantalla1_0494.dart
@@ -12,20 +12,8 @@ class Pantalla1_0494 extends StatelessWidget {
           title: const Text('card p1 Galindo_0494'),
           backgroundColor: const Color(0xff7751ff)),
       body: Center(
-        child: Card(
-          elevation: 20,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          color: Color(0xff7751ff),
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              'Card Galindo',
-              style: TextStyle(fontSize: 30, color: Color(0xff02fff2)),
+
             ),
-          ),
-        ),
-      ),
-    );
+    )
   }
 }
